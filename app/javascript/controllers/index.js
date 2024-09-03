c62a6d83a8a032f0ee1c050521924732009f4ff6
Kuxixima Almeida <kuxialmeida@hotmail.com>
2024-09-03 01:01:21 +0100

    arrumando chatroom

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,12 @@
 
 import { application } from "controllers/application"
 
+import ChatroomSubscriptionController from "./chatroom_controller.js"
+application.register("chatroom-subscription", ChatroomSubscriptionController)
+
+import HelloController from "./hello_controller.js"
+application.register("hello", HelloController)
+
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
