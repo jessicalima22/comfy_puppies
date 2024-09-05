f2649a507a2a5ef227f182efeeb34935f7ad4c27
Kuxixima Almeida <kuxialmeida@hotmail.com>
2024-09-05 23:11:15 +0100

    about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'chatrooms/index'
-  get 'chatrooms/show'
-  get 'chatrooms/new'
-  get 'chatrooms/create'
+  get 'about', to: 'pages#about'
+  
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
