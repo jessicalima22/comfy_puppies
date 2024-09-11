9e539c4330b30c002aadaaf209d0e572a0328e6e
monikahackmayer <mhackmayer@gmail.com>
2024-09-10 21:39:29 -0300

    atualizando de novo

--- db/seeds.rb
+++ db/seeds.rb
@@ -316,6 +316,7 @@ Animal.create!(
       filename: "fluffy_photo1.jpg",
       content_type: 'image/jpeg'
     ),
+    
     ActiveStorage::Blob.create_and_upload!(
       io: URI.open("https://res.cloudinary.com/dsoletyk5/image/upload/v1725717698/pete-f-GpLEzl4OthA-unsplash_xzc2tv.jpg"),
       filename: "fluffy_photo2.jpg",
